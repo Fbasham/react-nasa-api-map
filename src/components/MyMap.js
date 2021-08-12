84b9ada48bfede954fd8e50c85d92e68eaae01f1
Basham <frankabasham@gmail.com>
2021-08-12 10:55:45 -0400

    add file

--- src/components/MyMap.js
+++ src/components/MyMap.js
@@ -13,28 +13,30 @@ const MyMap = ({ events }) => {
   }, [show]);
 
   return (
-    <div className="map">
-      <Map defaultCenter={[45, -93]} defaultZoom={4} minZoom={3}>
-        {events
-          .filter((e) => e.categories[0].id === 8)
-          .map((e) => {
-            const id = e.id;
-            const title = e.title;
-            const date = e.geometries[0].date;
-            const lat = e.geometries[0].coordinates[1];
-            const lng = e.geometries[0].coordinates[0];
-            return (
-              <Overlay key={e.id} anchor={[lat, lng]}>
-                <Marker
-                  active={info?.id === e.id}
-                  onClick={() => setInfo({ id, title, date, lat, lng })}
-                />
-              </Overlay>
-            );
-          })}
-      </Map>
+    <>
+      <div className="map" onClick={(e)=>e.target.className==='pigeon-overlays' ? setShow(false) : null}>
+        <Map defaultCenter={[45, -93]} defaultZoom={4} minZoom={3}>
+          {events
+            .filter((e) => e.categories[0].id === 8)
+            .map((e) => {
+              const id = e.id;
+              const title = e.title;
+              const date = e.geometries[0].date;
+              const lat = e.geometries[0].coordinates[1];
+              const lng = e.geometries[0].coordinates[0];
+              return (
+                <Overlay key={e.id} anchor={[lat, lng]}>
+                  <Marker
+                    active={info?.id === e.id}
+                    onClick={() => setInfo({ id, title, date, lat, lng })}
+                  />
+                </Overlay>
+              );
+            })}
+        </Map>
+      </div>
       {show && info && <Info info={info} onClick={(e) => setShow(!show)} />}
-    </div>
+    </>
   );
 };
 
