3260374a9fe4252382109882fa729e18bcc51bff
Frank <48450599+Fbasham@users.noreply.github.com>
2022-05-02 09:44:00 -0400

    Update Marker.js

--- src/components/Marker.js
+++ src/components/Marker.js
@@ -1,7 +1,6 @@
 import { RiFireFill } from "react-icons/ri";
 
 const Marker = ({ active, onClick }) => {
-  console.log()
   return (
     <div className={`marker ${Boolean(active) ? 'active' : ''}`} onClick={onClick}>
       <RiFireFill />
